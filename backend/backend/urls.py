39058c0b3020621a8543f6347ddb160bea92ee0d
Gabe Richard <gabe_rch@hotmail.com>
2019-08-24 18:31:35 -0400

    Added first api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from makeitcount import views
+
+router = routers.DefaultRouter()
+router.register(r'activities', views.ActivityView, 'Activity')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
